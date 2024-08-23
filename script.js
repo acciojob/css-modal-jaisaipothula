3f156b4c275e7437c3348f9a0501b2640f60bda2
POTHULA. JAI SAI <92910772+jaisaipothula@users.noreply.github.com>
2024-08-23 14:00:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+// Get modal elements
+var modal = document.getElementById("modal");
+var btn = document.getElementById("openModal");
+var span = document.getElementById("closeModal");
+
+// Open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// Close the modal when the user clicks on <span> (x)
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Close the modal when the user clicks anywhere outside of the modal
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
